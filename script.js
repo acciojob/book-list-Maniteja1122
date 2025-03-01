0e224386c5c814a7cbe34a82a5fd1ac57f3446c7
MANITEJA KUNTA <129951439+Maniteja1122@users.noreply.github.com>
2025-03-01 13:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let inputName = document.getElementById("name")
+let authorInp = document.getElementById("author")
+let isbninp = document.getElementById("isbn")
+let btn = document.getElementById("button")
+let tbody = document.querySelector("#body")
+let arr =[]
+console.log(btn,)
+btn.addEventListener("click" , ()=>{
+	let obj ={
+		name : inputName.value,
+		Author:authorInp.value,
+		ISBN : isbninp.value
+	}
+	arr.push(obj)
+    
+		
+	let tr = document.createElement("tr") 
+	let title = document.createElement("td")
+	let author = document.createElement("td")
+	let isb = document.createElement("td")
+		
+		title.textContent=obj.name
+		author.textContent = obj.Author
+		isb.textContent = obj.ISBN
+
+		tr.append(title);
+		tr.append(author);
+		tr.append(isb);
+	console.log(obj);
+        tbody.append(tr);
+});
